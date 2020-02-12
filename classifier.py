7ac886280264f6ca30ff183105755e4f56bf26a3
bargav <bj4nq@virginia.edu>
2020-02-12 10:56:45 -0500

    added gdp multiplier for 30 epochs

--- classifier.py
+++ classifier.py
@@ -12,13 +12,13 @@ LOG_DIR = 'log'
 AdamOptimizer = tf.compat.v1.train.AdamOptimizer
 
 # optimal sigma values for RDP mechanism for the batch size = 200, epochs = 100, training set size = 10000, delta = 1e-5.
-noise_multiplier = {0.01:525, 0.05:150, 0.1:70, 0.5:13.8, 1:7, 5:1.669, 10:1.056, 50:0.551, 100:0.445, 500:0.275, 1000:0.219}
+#noise_multiplier = {0.01:525, 0.05:150, 0.1:70, 0.5:13.8, 1:7, 5:1.669, 10:1.056, 50:0.551, 100:0.445, 500:0.275, 1000:0.219}
 # optimal sigma values for GDP mechanism for the batch size = 200, epochs = 100, training set size = 10000, delta = 1e-5.
-gdp_noise_multiplier = {0.01:350, 0.05:82, 0.1:44, 0.5:10, 1:5.4, 5:1.43, 10:0.955, 50:0.564, 100:0.498}
+#gdp_noise_multiplier = {0.01:350, 0.05:82, 0.1:44, 0.5:10, 1:5.4, 5:1.43, 10:0.955, 50:0.564, 100:0.498}
 # optimal sigma values for RDP mechanism for the batch size = 200, epochs = 30, training set size = 10000, delta = 1e-5.
-#noise_multiplier = {0.1:36, 0.5:7.6, 1:3.9, 5:1.1, 10:0.79, 50:0.445, 100:0.356}
+noise_multiplier = {0.01: 290, 0.05:70, 0.1:36, 0.5:7.6, 1:3.9, 5:1.1, 10:0.79, 50:0.445, 100:0.356, 500:0.206, 1000:0.157}
 # optimal sigma values for GDP mechanism for the batch size = 200, epochs = 30, training set size = 10000, delta = 1e-5.
-#gdp_noise_multiplier = {0.1:24, 0.5:5.5, 1:3, 5:0.94, 10:0.701, 50:0.481, 100:0.438}
+gdp_noise_multiplier = {0.01:190, 0.05:45, 0.1:24, 0.5:5.5, 1:3, 5:0.94, 10:0.701, 50:0.481, 100:0.438}
 
 def get_predictions(predictions):
     pred_y, pred_scores = [], []
