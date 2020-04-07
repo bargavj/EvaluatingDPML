7e5756794c39280d9f403fffb77d13effc652d91
bargav <bj4nq@virginia.edu>
2020-04-07 14:01:45 -0400

    added slurm script

--- run_script_per_crawler_slurm.sh
+++ run_script_per_crawler_slurm.sh
@@ -2,20 +2,30 @@
 
 #SBATCH --job-name="DiPrivML"
 #SBATCH --ntasks=100
+#SBATCH --gres=gpu:4
 
 echo "Starting script"
 if [[ $# -eq 0 ]] ; then # if called with no arguments
-    echo "Usage: sbatch $0 <DATASET NAME>"
+    echo "Usage: bash $0 [-s] <DATASET NAME> <RUN>"
+    echo "  -s: saves new sample of dataset to data/ directory"
     exit 1
 fi
 
-DATASET=$1
+if [ $1 == '-s' ]; then
+    DATASET=$2
+    SAVE_DATA=true
+    RUN=$3
+else
+    DATASET=$1
+    SAVE_DATA=false
+    RUN=$2
+fi
+
 ATTACK_PY=attack.py
 
 echo "Loading modules"
 source /etc/profile.d/modules.sh
 module load anaconda3
-module load parallel
 
 # Make sure conda environment has dependencies
 echo "Creating conda environment"
@@ -25,36 +35,30 @@ source activate slurm
 conda install -y pip
 conda install -y python
 
-conda create -n tf tensorflow
-source activate tf
+conda create -n tf-gpu tensorflow-gpu
+source activate tf-gpu
 conda install scikit-learn
 
 pip install dm-tree
 pip install matplotlib
 pip install git+git://github.com/tensorflow/privacy@master
 
-echo "Filling data/ directory"
-python $ATTACK_PY $DATASET --use_cpu=1 --save_data=1 --target_test_train_ratio=10
+if [ "$SAVE_DATA" = true ]; then
+    echo "Filling data/ directory"
+    python $ATTACK_PY $DATASET --use_cpu=0 --save_data=1 --target_test_train_ratio=10
+fi
 
 echo "Beginning experiment"
-# parallel will run the quoted command, replacing each {n} with each value on
-# the n-th line starting with ":::" exactly as if it were a nested for-loop.
-# The start-times, durations, and commands of tasks are stored in joblog.txt.
-parallel -j 20 "srun --exclusive -N1 -n1 python $ATTACK_PY $DATASET \
-    --target_privacy='no_privacy' \
-    --use_cpu=1 \
-    --target_epochs=100 \
-    --target_learning_rate=0.005 \
-    --target_test_train_ratio={1} \
-    --run={2}" ::: 1 2 10 ::: 1 2 3 4 5
-parallel -j $SLURM_NTASKS --joblog joblog.txt --ungroup \
-    "srun --exclusive -N1 -n1 python $ATTACK_PY $DATASET \
-    --target_privacy='grad_pert' \
-    --use_cpu=1 \
-    --target_epochs=100 \
-    --target_learning_rate=0.005 \
-    --target_test_train_ratio={1} \
-    --target_dp={2} \
-    --target_epsilon={3} \
-    --run={4}" ::: 1 2 10 ::: 'gdp' 'rdp' ::: 0.1 0.5 1.0 5.0 10.0 50.0 100.0 ::: 1 2 3 4 5
+for GAMMA in 1 2 10
+    do
+    python $ATTACK_PY $DATASET --use_cpu=0 --target_test_train_ratio=$GAMMA --target_epochs=100 --target_learning_rate=0.005 --target_privacy='no_privacy' --run=$RUN
+    for EPSILON in 0.1 0.5 1.0 5.0 10.0 50.0 100.0 500.0 1000.0
+    do
+        python $ATTACK_PY $DATASET --use_cpu=0 --target_test_train_ratio=$GAMMA --target_epochs=100 --target_learning_rate=0.005 --target_privacy='grad_pert' --target_dp='rdp' --target_epsilon=$EPSILON --run=$RUN
+    done
+    for EPSILON in 0.1 0.5 1.0 5.0 10.0 50.0 100.0
+    do
+        python $ATTACK_PY $DATASET --use_cpu=0 --target_test_train_ratio=$GAMMA --target_epochs=100 --target_learning_rate=0.005 --target_privacy='grad_pert' --target_dp='gdp' --target_epsilon=$EPSILON --run=$RUN
+    done
+done
 echo done
