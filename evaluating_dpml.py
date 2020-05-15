e4ddb1194c9412c505eaa0ca1b6d51d790d45482
bargavj <bj4nq@virginia.edu>
2020-05-15 18:04:44 -0400

    Update evaluating_dpml.py

--- evaluating_dpml.py
+++ evaluating_dpml.py
@@ -48,15 +48,19 @@ def run_experiment(args):
     shokri_mem_adv, shokri_mem_confidence = shokri_membership_inference(args, pred_y, membership, test_classes)
 
     # Yeom's attribute inference attack when train_loss is known - Adversary 4 of Yeom et al.
-    yeom_ai_outputs_1 = yeom_attribute_inference(true_x, true_y, classifier, membership, features, train_loss)
+    pred_membership_all = yeom_attribute_inference(true_x, true_y, classifier, membership, features, train_loss)
+    yeom_attr_adv = []
+    for pred_membership in pred_membership_all:
+        fpr, tpr, thresholds = roc_curve(membership, pred_membership, pos_label=1)
+        yeom_attr_adv.append(tpr[1] - fpr[1])
     
     if not os.path.exists(RESULT_PATH+args.train_dataset):
         os.makedirs(RESULT_PATH+args.train_dataset)
     
     if args.target_privacy == 'no_privacy':
-        pickle.dump([train_acc, test_acc, train_loss, membership, shokri_mem_adv, shokri_mem_confidence, yeom_mem_adv, per_instance_loss, yeom_attr_adv, yeom_attr_mem, yeom_attr_pred, features], open(RESULT_PATH+args.train_dataset+'/'+args.target_model+'_'+'no_privacy_'+str(args.l2_ratio)+'.p', 'rb'))
+        pickle.dump([train_acc, test_acc, train_loss, membership, shokri_mem_adv, shokri_mem_confidence, yeom_mem_adv, per_instance_loss, yeom_attr_adv, pred_membership_all, features], open(RESULT_PATH+args.train_dataset+'/'+args.target_model+'_'+'no_privacy_'+str(args.l2_ratio)+'.p', 'rb'))
     else:
-        pickle.dump([train_acc, test_acc, train_loss, membership, shokri_mem_adv, shokri_mem_confidence, yeom_mem_adv, per_instance_loss, yeom_attr_adv, yeom_attr_mem, yeom_attr_pred, features], open(RESULT_PATH+args.train_dataset+'/'+args.target_model+'_'+args.target_privacy+'_'+args.target_dp+'_'+str(args.target_epsilon)+'_'+str(args.run)+'.p', 'wb'))
+        pickle.dump([train_acc, test_acc, train_loss, membership, shokri_mem_adv, shokri_mem_confidence, yeom_mem_adv, per_instance_loss, yeom_attr_adv, pred_membership_all, features], open(RESULT_PATH+args.train_dataset+'/'+args.target_model+'_'+args.target_privacy+'_'+args.target_dp+'_'+str(args.target_epsilon)+'_'+str(args.run)+'.p', 'wb'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
